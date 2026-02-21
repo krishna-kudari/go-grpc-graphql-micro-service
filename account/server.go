53754f5e6dce312544e0c89126ac4253698152e5
Krishna Kudari <kkrishna22572@gmail.com>
2026-02-21 22:54:43 +0530

    Add initial account service implementation with Docker support and Go module setup

---
+++ account/server.go
@@ -0,0 +1,51 @@
+package account
+
+import (
+	"context"
+	"fmt"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type grpcServer struct {
+	service Service
+}
+
+func ListenGRPC(s Service, port int) error {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		return err
+	}
+	serv := grpc.NewServer()
+	// pb.(serv, )
+	reflection.Register(serv)
+	return serv.Serve(lis)
+}
+
+func (s *grpcServer) PostAccount(ctx context.Context, r *pb.) (*pb., error) {
+	a, err := s.service.PostAccount(ctx, r.Name)
+	if err != nil {
+		return nil, err
+	}
+	return &pb.{}
+}
+
+
+func (s *grpcServer) GetAccount(ctx context.Context, r *pb.) (*pb., error) {
+	a, err := s.service.GetAccount(ctx, r.Id)
+	if err != nil {
+		return nil, err
+	}
+	return &pb.{}
+}
+
+
+func (s *grpcServer) GetAccounts(ctx context.Context, r *pb.) (*pb., error) {
+	a, err := s.service.GetAccounts(ctx, r.skip, r.take)
+	if err != nil {
+		return nil, err
+	}
+	return &pb.{}
+}
