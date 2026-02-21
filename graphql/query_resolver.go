f41b152bbd586a635b59f4afa8b9a9b7bc5929bf
Krishna Kudari <kkrishna22572@gmail.com>
2026-02-21 22:54:29 +0530

    Initialize GraphQL server with account, product, and order management functionality. Added resolvers, models, and schema definitions for handling accounts, products, and orders. Implemented server setup and configuration for GraphQL endpoints.

---
+++ graphql/query_resolver.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"context"
+	"strings"
+)
+
+type queryResolver struct {
+	server *Server
+}
+
+func (r *queryResolver) Accounts(ctx context.Context, pagination *PaginationInput, id *string)([]*Account, error)  {
+
+}
+
+func (r *queryResolver) Products(ctx context.Context, pagination *PaginationInput, query *string, id *string)([]*Product, error)  {
+
+}
+
